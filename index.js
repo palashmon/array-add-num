c5bdc34ac686343265a365c3010f4d9d6b24e231
Palash Mondal <palashm@live.in>
2023-04-18 13:20:32 +0530

    refactor: Update code with jsdocs and update tests.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-module.exports = (input = []) => {
-	if (!Array.isArray(input)) {
-		throw new TypeError(`Expected an array, got ${typeof input}`);
-	}
+/**
+ * Computes the sum of all finite numbers in the given array.
+ *
+ * @param {Array<unknown>=} input The input array to compute the sum. Optional, defaults to an empty array.
+ * @throws {TypeError} If the input parameter is not an array.
+ * @returns {number|TypeError} The computed sum of all finite numbers in the array, or a TypeError if the input is not an array.
+ */
+function arrayAdd(input = []) {
+  if (!Array.isArray(input)) {
+    throw new TypeError(`Expected an array, got ${typeof input}`);
+  }
 
-	return input
-		.filter(Boolean)
-		.filter(v => !isNaN(v) && isFinite(v))
-		.reduce((ac, curr) => ac + Number(curr), 0);
-};
+  const filteredArray = input.filter((value) => Number.isFinite(Number(value)));
+
+  let sum = 0;
+  for (const value of filteredArray) {
+    sum += Number(value);
+  }
+
+  return sum;
+}
+
+export default arrayAdd;
--- test.js
+++ test.js
@@ -1,58 +1,67 @@
-import test from 'ava';
-import arrayAdd from '.';
+import test from "ava";
+import arrayAdd from "./index.js";
 
-test('Return typeerror when input is not an array.', t => {
-	const err = t.throws(() => {
-		arrayAdd(23);
-	}, TypeError);
-	t.is(err.message, 'Expected an array, got number');
+test("Return typeerror when input is not an array.", (t) => {
+  const error = t.throws(() => arrayAdd(23));
+  t.is(error.message, "Expected an array, got number");
 });
 
-test('Return 0 when an array is empty.', t => {
-	t.deepEqual(arrayAdd([]), 0);
-	t.deepEqual(arrayAdd(), 0);
+test("Return 0 when an array is empty.", (t) => {
+  t.is(arrayAdd([]), 0);
+  t.is(arrayAdd(), 0);
 });
 
-test('Sum up all the numbers in the array.', t => {
-	t.deepEqual(arrayAdd([1, 2, 3, 4]), 10);
+test("Sum up all the numbers in the array.", (t) => {
+  t.is(arrayAdd([1, 2, 3, 4]), 10);
 });
 
-test('Sum up only numbers in the array.', t => {
-	t.deepEqual(arrayAdd([1, 'abc', 3, 4, {}, 2, null]), 10);
+test("Sum up only numbers in the array.", (t) => {
+  t.is(arrayAdd([1, "abc", 3, 4, {}, 2, null]), 10);
 
-	// Using decimal values
-	t.deepEqual(arrayAdd([10, 1.5]), 11.5);
-	t.deepEqual(arrayAdd([10, '1.5']), 11.5);
+  // Using decimal values
+  t.is(arrayAdd([10, 1.5]), 11.5);
+  t.is(arrayAdd([10, "1.5"]), 11.5);
 
-	// Using -ve values
-	t.deepEqual(arrayAdd([10, -1]), 9);
-	t.deepEqual(arrayAdd([10, '-1']), 9);
+  // Using -ve values
+  t.is(arrayAdd([10, -1]), 9);
+  t.is(arrayAdd([10, "-1"]), 9);
 
-	// Using -ve decimal values
-	t.deepEqual(arrayAdd([10, -1.1]), 8.9);
-	t.deepEqual(arrayAdd([10, '-1.1']), 8.9);
+  // Using -ve decimal values
+  t.is(arrayAdd([10, -1.1]), 8.9);
+  t.is(arrayAdd([10, "-1.1"]), 8.9);
 
-	// Using string with 0 prefix
-	t.deepEqual(arrayAdd(['0', 1, 2]), 3);
-	t.deepEqual(arrayAdd(['01', 1, 2]), 4);
+  // Using string with 0 prefix
+  t.is(arrayAdd(["0", 1, 2]), 3);
+  t.is(arrayAdd(["01", 1, 2]), 4);
 
-	// Using hex values, 0xAB = 171
-	t.deepEqual(arrayAdd(['0xAB', 1]), 172);
-	t.deepEqual(arrayAdd(['0xAB', 1, '0xAC']), 344);
+  // Using hex values, 0xAB = 171
+  t.is(arrayAdd(["0xAB", 1]), 172);
+  t.is(arrayAdd(["0xAB", 1, "0xAC"]), 344);
 
-	// Using exponential values
-	t.deepEqual(arrayAdd([1e3, 1, 2, 3]), 1006);
-	t.deepEqual(arrayAdd(['1e3', 1, 2, 3]), 1006);
+  // Using exponential values
+  t.is(arrayAdd([1e3, 1, 2, 3]), 1006);
+  t.is(arrayAdd(["1e3", 1, 2, 3]), 1006);
 });
 
-test('Sum up integer strings in the array.', t => {
-	t.deepEqual(arrayAdd(['1', '2', '3', '4']), 10);
+test("Sum up integer strings in the array.", (t) => {
+  t.is(arrayAdd(["1", "2", "3", "4"]), 10);
 });
 
-test('Sum up integer strings & numbers mix in the array.', t => {
-	t.deepEqual(arrayAdd(['1', 2, '3', 4]), 10);
+test("Sum up integer strings & numbers mix in the array.", (t) => {
+  t.is(arrayAdd(["1", 2, "3", 4]), 10);
 });
 
-test('Sum up only finite numbers.', t => {
-	t.deepEqual(arrayAdd([1, 2, 3, 4, Infinity, NaN, -Infinity]), 10);
+test("Sum up only finite numbers.", (t) => {
+  t.is(
+    arrayAdd([
+      1,
+      2,
+      3,
+      4,
+      Number.POSITIVE_INFINITY,
+      Number.NaN,
+      Number.NEGATIVE_INFINITY,
+    ]),
+    10
+  );
 });
